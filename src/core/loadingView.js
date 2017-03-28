054c8c8f427edad871d439621a7adb10d3ceea0f
Kent C. Dodds <kent+github@doddsfamily.us>
2017-03-28 06:46:35 -0600

    delay rendering loading

--- src/core/loadingView.js
+++ src/core/loadingView.js
@@ -1,7 +1,21 @@
-import {h} from 'preact';
+import {h, Component} from 'preact';
 import Logo from '../icons/logo.js';
 import styles from './routeLoading.css';
 
-export default function LoadingView() {
-  return <div class={styles.routeLoading}><Logo width={60} height={60} /></div>;
+export default class LoadingView extends Component {
+  state = {render: false};
+  componentDidMount() {
+    const reasonableDelay = 200;
+    setTimeout(
+      () => {
+        this.setState({render: true});
+      },
+      reasonableDelay
+    );
+  }
+  render() {
+    return this.state.render
+      ? <div class={styles.routeLoading}><Logo width={60} height={60} /></div>
+      : null;
+  }
 }
